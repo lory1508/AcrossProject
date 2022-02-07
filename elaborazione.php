c980e33183dc9c3a6eed85b91233005b2e9fd0a6
lory1508 <lorenzo.galassi91@gmail.com>
2022-02-07 22:27:32 +0100

    Step 2 completed
    Completata implementazione step 2

--- elaborazione.php
+++ elaborazione.php
@@ -1,25 +1,38 @@
 <?php
 
 $nomeApplicazione = "AcrossProject";
+$token = "@";
 
-$word = $_GET['parola'];
+$word = isset($_GET['parola']) ? $_GET['parola'] : '';
 
-if (validate($word)) {
-	if (isPalindroma($word))
-        writeLog("evviva, la parola <$word> è palindroma");
-    else
-        writeLog("$word non è palindroma");
+if(!empty($word)){
+    if (validate($word)) {
+        $word_array = explode($token, $word);
+        foreach($word_array as $w){
+	        if (isPalindroma($w))
+                writeLog("evviva, la parola <$w> è palindroma");
+            else
+                writeLog("$w non è palindroma");
+        }
+    }
+    else {
+        writeLog("ERRORE, parola inserita: $word");
+    }
 }
-else {
-    writeLog("ERRORE, parola inserita: $word");
-}
-
 
 function validate($word){
-    $word_array = explode(" ", $word);
-    if (count($word_array) > 1)
+    global $token;
+
+    //controllo se sono presenti due token consecutivi
+    $re = '/.*@{2}.*/';
+    preg_match_all($re, $word, $matches, PREG_SET_ORDER, 0);
+    if(!empty($matches)) return false;
+
+    //controllo se sono presenti più di due token nella stringa inserita
+    $word_array = explode($token, $word);
+    if (count($word_array) > 3)
         return false;
-    return ctype_alnum($word);
+    return true;
 }
 
 function isPalindroma($word){
@@ -35,12 +48,13 @@ function isPalindroma($word){
 function writeLog($log) {
     global $nomeApplicazione;
     $date = date("dmY");
+    $date_inner = date("d/m/Y");
     $log_location = "logs";
     if (!file_exists($log_location)) 
        mkdir($log_location, 0777, true);
     
     $log_file_data = $log_location.'/' . $date . "-" . $nomeApplicazione . '.log';
 
-    file_put_contents($log_file_data, $log. "\n", FILE_APPEND);
+    file_put_contents($log_file_data, "[$date_inner] ".$log. "\n", FILE_APPEND);
 } 
 ?>
